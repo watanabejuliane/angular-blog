81d57b2d55a587c78cdcb30cbbd30114a3b10dd0
Juliane Watanabe B. Mendes <157089261+watanabejuliane@users.noreply.github.com>
2024-04-22 10:03:18 +0900

    style completed

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,50 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "photoCover":"https://i.pinimg.com/564x/78/73/57/787357291f5a41fa0ed996830df33f29.jpg",
+    "title": "Mochi: Delicacy in Every Bite",
+    "description": "Mochi, a quintessential Japanese delicacy, holds a special place in Japanese culinary traditions. Made from glutinous rice pounded into a sticky, chewy texture, mochi comes in various shapes and sizes, often filled with sweet red bean paste or flavored with ingredients like matcha or kinako (toasted soybean flour). Its versatility shines through in both traditional and modern desserts, from the classic daifuku to innovative creations like mochi ice cream. Whether enjoyed during celebratory occasions or as a daily indulgence, mochi embodies the essence of Japanese craftsmanship and culinary creativity."
+  },
+  {
+    "id":"2",
+    "photoCover":"https://i.pinimg.com/564x/ba/67/69/ba6769c4a356103a8c984bf97beb0c77.jpg",
+    "title": "Anmitsu: A Colorful Delight of Japanese Desserts",
+    "description": "Anmitsu is a delightful Japanese dessert consisting of agar jelly cubes, sweet azuki bean paste, fruits, and kuromitsu syrup. Its refreshing flavors and vibrant colors create a harmonious blend of sweetness and texture, making it a popular choice during the hot summer months. Anmitsu's origins date back to the Edo period, where it was enjoyed as a luxurious treat by nobility. Today, it continues to be cherished for its simple yet elegant presentation, offering a taste of Japanese tradition with every spoonful."
+  },
+  {
+    "id":"3",
+    "photoCover":"https://radarbanyumas.disway.id/upload/931607f047703f71a435029d8531eba3.jpeg",
+    "title": "Dorayaki: Doraemon's Favorite Treat",
+    "description": "Dorayaki, a sweet pancake sandwich filled with velvety sweet red bean paste, is a cherished favorite in Japan. Its soft, fluffy pancakes encase a rich, creamy filling, offering a delightful contrast of textures and flavors. Often associated with childhood memories and beloved characters like Doraemon, dorayaki has captured the hearts of people of all ages. While traditional anko filling remains a classic choice, modern variations such as matcha, chocolate, and custard have expanded the possibilities of this beloved Japanese treat."
+  },
+  {
+    "id":"4",
+    "photoCover":"https://i.pinimg.com/736x/56/3b/0d/563b0df3c714d816973e0f1ba220f2a4.jpg",
+    "title": "Wagashi: Edible Japanese Art",
+    "description": "Wagashi, traditional Japanese sweets, are not only delectable treats but also works of art that reflect Japan's rich cultural heritage. These intricately crafted confections are often enjoyed during tea ceremonies, seasonal celebrations, and other special occasions. With a focus on natural ingredients and meticulous craftsmanship, wagashi come in various forms, including yokan (bean jelly), namagashi (fresh seasonal sweets), and higashi (dry sweets). Each wagashi offers a sensory experience that celebrates the beauty of nature and the elegance of Japanese aesthetics."
+  },
+  {
+    "id":"5",
+    "photoCover":"https://kodawari-times.net/?attachment_id=1936",
+    "title": "Manju: Japanese Sweet Treat",
+    "description": "Manju, a classic Japanese confection, holds a special place in Japanese culinary culture. These soft, pillowy treats are made from a dough of flour, rice powder, and sugar, filled with a sweet bean paste or other fillings. Manju comes in various shapes and sizes, often resembling small buns or dumplings. Its subtle sweetness and delicate texture make it a delightful indulgence, perfect for enjoying with a cup of tea or as a snack on the go. Whether enjoyed as a traditional treat or in modern interpretations with innovative fillings, manju embodies the essence of Japanese hospitality and craftsmanship, making it a beloved favorite among locals and visitors alike."
+  },
+  {
+    "id":"6",
+    "photoCover":"https://i.pinimg.com/564x/81/fd/d3/81fdd3d1e13558c8a78de9b1031c42c9.jpg",
+    "title": "Dango: Bite-Sized Delights from Japan",
+    "description": "Dango, small rice dumplings skewered on a stick, are a charming and beloved Japanese sweet. Made from a mixture of rice flour and glutinous rice flour, dango boasts a delightfully chewy texture that pairs perfectly with its sweet toppings. Traditionally enjoyed during seasonal celebrations like hanami (cherry blossom viewing) and mochitsuki (rice cake pounding), dango comes in various flavors such as mitarashi (sweet soy sauce), kinako (toasted soybean flour), and anko (sweet red bean paste). With its simplicity and versatility, dango has earned its place as a cherished treat in Japanese cuisine."
+  },
+  {
+    "id":"7",
+    "photoCover":"https://shotengai.com/cdn/shop/articles/banner_61b46acc-4b53-46b2-91af-8cdd8e61eda7.jpg?v=1698400301",
+    "title": "Yokan: The Elegant Japanese Jelly",
+    "description": "Yokan, a traditional Japanese sweet jelly made from agar, sugar, and sweet bean paste, exudes simplicity and elegance. Its smooth texture and subtle sweetness make it a delightful treat to enjoy during tea ceremonies and special occasions. Yokan comes in various flavors, including classic red bean, matcha, and chestnut, each offering a unique sensory experience. With its rich cultural history and timeless appeal, yokan remains a beloved symbol of Japanese confectionery craftsmanship."
+  },
+  {
+    "id":"8",
+    "photoCover":"https://www.japanesecooking101.com/wp-content/uploads/2018/07/DSC00302.jpg",
+    "title": "Kakigori: Shaved Ice Elegance",
+    "description": "Kakigori, a beloved Japanese dessert, offers a refreshing escape from the summer heat with its finely shaved ice and flavorful syrups. Its light and fluffy texture create a delicate canvas for a variety of toppings, including traditional flavors like strawberry, matcha, and condensed milk. Kakigori holds a special place in Japanese culture, with festivals and vendors offering a wide array of creative twists on this classic treat. Whether enjoyed at a street stall or homemade with friends, kakigori brings joy and refreshment to all who indulge in its icy sweetness."
+  }
+]
